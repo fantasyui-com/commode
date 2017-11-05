baed01fe9c988a494d067cf5e066e22e85f0222f
Captain Fantasy <captain@fantasyui.com>
2017-11-05 12:00:38 -0500

    List Selection and Filtering

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # commode
 Electron-Vue based, iTunes like UI, for file tagging, restructuring, processing, and general management.
+
+![](screenshot.png)
--- renderer.js
+++ renderer.js
@@ -4,21 +4,74 @@
 
 new Vue({
   el: '#primary',
+
   data: {
-    items: [
-      { name: 'Chuck Norris', power: Infinity },
-      { name: 'Bruce Lee', power: 9000 },
-      { name: 'Jackie Chan', power: 7000 },
-      { name: 'Jet Li', power: 8000 }
-    ]  },
-  computed: {
-    things: function () {
-      return this.items;
-    }
+    filter: i=> i.power >= 8000,
+    filters: [],
+    db: [],
   },
+
+  created: function () {
+    this.deselect();
+    this.update();
+  },
+
+  computed: {
+
+    computedFilters: function () {
+      return this.filters;
+    },
+
+    computedDatabase: function () {
+      return this.db.filter( this.filter );
+    },
+
+  },
+
   methods: {
-    update: _.debounce(function (e) {
-      this.input = e.target.value
-    }, 300)
+
+    select:  function (e) {
+      this.filter = e.filter;
+    },
+
+    deselect:  function (e) {
+      this.filter = i => true;
+    },
+
+    update:  function (e) {
+
+
+        [
+          { name: 'Radio',      filter: i => i.kind.indexOf('Radio')!= -1 },
+          { name: 'Audiobooks', filter: i => i.kind.indexOf('Audiobook')!=-1 },
+          { name: 'Music',      filter: i => i.kind.indexOf('Music')!= -1 },
+          { name: 'Movies',     filter: i => i.kind.indexOf('Movies')!=-1 },
+        ].forEach( i => this.filters.push(i) );
+
+        [
+          { name: 'Podcast with Ads', kind: ['Radio']},
+          { name: 'Man\'s Search for Meaning', kind: ['Audiobook']},
+          { name: 'Wanted', kind: ['Movies']},
+
+
+          { name: 'Wolfsheim - Once In A Lifetime', kind: ['Music']},
+          { name: 'Depeche Mode - Enjoy The Silence', kind: ['Music']},
+          { name: 'Apoptygma Berzerk - Kathy\'s Song', kind: ['Music']},
+          { name: 'And One - Wasted', kind: ['Music']},
+          { name: 'Blaq Audio - Stiff Kitttens', kind: ['Music']},
+          { name: 'Stromkern - Stand Up', kind: ['Music']},
+          { name: 'Rotersand - I Don\'t Know', kind: ['Music']},
+          { name: 'Ladytron - Destroy Everything You Touch', kind: ['Music']},
+          { name: 'Bjork - Army of Me', kind: ['Music']},
+          { name: 'Nine Inch Nails - Closer', kind: ['Music']},
+
+
+
+
+        ].forEach( i => this.db.push(i) );
+
+
+    },
   }
+
 })

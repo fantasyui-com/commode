6bb440d2756ae5d65f9627a9fae8cf46eb4f3f4a
Captain Fantasy <captain@fantasyui.com>
2017-11-09 11:54:22 -0500

    Multi-tag Filter Example

--- renderer.js
+++ renderer.js
@@ -59,6 +59,7 @@ new Vue({
           { name: 'Audiobooks', filter: i => i.tags.indexOf('Audiobook')!=-1 },
           { name: 'Music',      filter: i => i.tags.indexOf('Music')!= -1 },
           { name: 'Movies',     filter: i => i.tags.indexOf('Movies')!=-1 },
+          { name: 'Radio & Audiobooks', filter: i => (['Radio','Audiobook'].map(j=>i.tags.indexOf(j)).filter(j=>j>-1).length>0) },
 
         ].forEach( i => this.filters.push(i) );
 
